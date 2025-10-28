aeb32079bfae4e4ae097dd671b98a457f0294ba6
Leo Zwindrecht <leozwindrecht@outlook.com>
2025-10-28 22:39:57 +0100

    css updated, html updated

--- cellularLife/obj/Debug/net9.0/cellularLife.AssemblyInfo.cs
+++ cellularLife/obj/Debug/net9.0/cellularLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ce8b1c3811596ab09ff07f58fb8f724bfdaf46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17309fe4ee6c77c3a9cdce3508272abdd1aabd7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
