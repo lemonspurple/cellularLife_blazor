663d6159923ef523b18e10e300da5a83224179f5
Leo Zwindrecht <leozwindrecht@outlook.com>
2025-11-17 21:56:23 +0100

    UI modifications. start site changed

--- cellularLife/obj/Debug/net9.0/cellularLife.AssemblyInfo.cs
+++ cellularLife/obj/Debug/net9.0/cellularLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c841d2930ad31bb5cf6636bf57fe7412c7421f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254d1a878efdf30e7b24f60cc4b0748b089b6c3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
