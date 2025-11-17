bf8ce84d9e295b022353721f6eb1391f67fbb156
Leo Zwindrecht <leozwindrecht@outlook.com>
2025-11-18 00:45:42 +0100

    CSS rework 2

--- cellularLife/obj/Debug/net9.0/cellularLife.AssemblyInfo.cs
+++ cellularLife/obj/Debug/net9.0/cellularLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254d1a878efdf30e7b24f60cc4b0748b089b6c3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+663d6159923ef523b18e10e300da5a83224179f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
