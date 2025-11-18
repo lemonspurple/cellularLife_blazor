788ff94e98a8230dbc79796e1918b49fd13ce83f
Leo Zwindrecht <leozwindrecht@outlook.com>
2025-11-18 01:53:26 +0100

    About site fixed link similar to ahref / navlink replacement in index/cellularlife.blazor

--- cellularLife/obj/Debug/net9.0/cellularLife.AssemblyInfo.cs
+++ cellularLife/obj/Debug/net9.0/cellularLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a35852cf1143f015b4ee4857686dc805de425c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b68b780b6d11fad9e667e9e52ae0499ec2c34c14")]
 [assembly: System.Reflection.AssemblyProductAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cellularLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
